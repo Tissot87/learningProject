c4fe257690481dd8485d371dbeb4ed83f78c9593
Kirill <kandriyanoff@yandex.ru>
2022-07-09 15:59:36 +0300

    Добавлены функции

--- js/script.js
+++ js/script.js
@@ -18,3 +18,38 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+
+function rememberMyFilms() {
+    for (i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов', ''),
+              b = prompt('На сколько его оцените', '');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+        } else {
+        console.log('error');
+        i--;
+        }
+    }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотренно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('err');
+    }
+
+}
+
+detectPersonalLevel();
+
+
+
+console.log(personalMovieDB);
